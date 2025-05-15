4432c56848cd3f364b1bd1f7d679127772f9ff74
lakenrollins <lakenrollins@gmail.com>
2025-05-14 21:18:10 -0500

    commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,23 +1,58 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+// backend/config/database.js
+const config = require('./index');
+
+module.exports = {
+  development: {
+    storage: config.dbFile,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    use_env_variable: '',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      schema: process.env.SCHEMA
+    }
   }
-}
+};
+
+
+
+
+
+
+// backend/config/database.js
+const config = require('./index');
+
+module.exports = {
+  development: {
+    storage: config.dbFile,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true
+  },
+  production: {
+    use_env_variable: 'postgresql://express_project_0482_user:ZliB9ALoHUxZ8LrjILnT8ZZZn5k7vBxI@dpg-d0d2tbmuk2gs73btk54g-a.oregon-postgres.render.com/express_project_0482',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      schema: process.env.SCHEMA
+    }
+  }
+};
